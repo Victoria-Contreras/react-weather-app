6af5beda42492a710782c0d5a5a10e23272a4e6d
Victoria <victoriahope5683@gmail.com>
2023-02-28 20:36:05 -0600

    nav bar and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
 const Navbar = () => {
     return ( 
         <nav>
-            <a href="#">Log out</a>
             <div id="logo"><h1 id='sun-logo'>SUN</h1><h1 id='shine-logo'>SHINE</h1></div>
+            <div id="links">
+            <a href="#">Favorites</a>
+            <a href="#">Log out</a>
+            </div>
+           
         </nav>
      );
 }
