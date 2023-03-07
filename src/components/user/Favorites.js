6a669c766d2ad2033e34de9a6f6b3a3d726c154c
Victoria <victoriahope5683@gmail.com>
2023-03-06 20:17:42 -0600

    favorites bar links working

--- src/components/user/Favorites.js
+++ src/components/user/Favorites.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { setCity } from '../../features/city/citySlice'
 import { Link } from 'react-router-dom'
 const Favorites = () => {
+    const dispatch = useDispatch();
     const [favorites, setFavorites] = useState([]);
 
     const getFavorites = () => {
@@ -19,11 +22,16 @@ const Favorites = () => {
         getFavorites()
     }, [favorites])
     
+    const handleClick = (e) => {
+        e.preventDefault()
+        dispatch(setCity(e.target.innerText))
+    }
+
     return ( 
         <div className='favorites-bar'>
             <ul>
                 <li>Favorites:</li>
-                {favorites.map((item, index) => <li key={index}><a href='#'>{item}</a></li>)}
+                {favorites.map((item, index) => <li key={index}><a href='#' onClick={(event) => handleClick(event)}>{item}</a></li>)}
             </ul>
 
         </div>
